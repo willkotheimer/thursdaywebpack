d7f7d583880f2ea809cd59a3657bda189a160d30
wkotheimer <wkotheimer@gmail.com>
2020-09-03 20:56:13 -0500

    Thursdat webpack push

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').html('<h1>Its thursday guys!</h1>');
+  console.log('Hey its Thursday!');
 };
 
 init();
